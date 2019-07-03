438bda3a8c4f3d694d479a90f7506ca5d83bca58
Lúcás Meier <cronokirby@gmail.com>
2019-07-03 18:54:46 +0200

    Add functionality to tag sentences into morphemes

---
+++ plugin.py
@@ -0,0 +1,20 @@
+from collections import namedtuple
+import MeCab
+
+
+Morpheme = namedtuple('Morpheme', ['raw', 'reading'])
+
+
+class Tagger:
+    def __init__(self):
+        self.mecab = MeCab.Tagger('-Ochasen')
+    
+    def tag(self, sentence):
+        acc = []
+        mecab_out = self.mecab.parse(sentence)
+        for line in mecab_out.split('\n'):
+            if line == 'EOS':
+                break
+            parts = line.split('\t')
+            acc.append(Morpheme(raw=parts[0], reading=parts[1]))
+        return acc
---
+++ plugin_test.py
@@ -0,0 +1,20 @@
+import unittest
+from plugin import *
+
+
+class TestTagger(unittest.TestCase):
+
+    def test_simple_sentences(self):
+        tagger = Tagger()
+        sentence = '猫が大好き。'
+        expected = [
+            Morpheme('猫', 'ネコ'),
+            Morpheme('が', 'ガ'),
+            Morpheme('大好き', 'ダイスキ'),
+            Morpheme('。', '。')
+        ]
+        self.assertEqual(tagger.tag(sentence), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
